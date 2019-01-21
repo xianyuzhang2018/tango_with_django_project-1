b7f31b7b0886da630c2ba73394034d5e28216dfc
Xianyu Zhang <792373462@qq.com>
2019-01-21 10:21:26 +0000

    static files

--- rango/views.py
+++ rango/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def about(request):
     return HttpResponse("Rango says this is about!  <br/> <a href='/rango/'>index</a>")
+
+def index(request):
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
