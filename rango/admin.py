dad14a16c55aad307cbc1535153df7a0610a0669
Xianyu Zhang <792373462@qq.com>
2019-01-23 22:54:12 +0000

    9.5 User profile

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
     prepolulated_fields = {'slug':('name',)}
@@ -10,4 +11,5 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
--- tango_with_jdango_project/settings.py
+++ tango_with_jdango_project/settings.py
@@ -96,6 +96,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
