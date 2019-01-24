baa4ad2ec83938636b12281a35cb4bdd09f649cf
Xianyu Zhang <792373462@qq.com>
2019-01-24 00:02:10 +0000

    9.6 register

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
-from rango.forms import PageForm
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 
 def index(request):
@@ -82,3 +80,36 @@ def add_page(request, category_name_slug):
 
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context_dict)
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+            user_form = UserForm(data=request.POST)
+            profile_form = UserProfileForm(data=request.POST)
+
+            if user_form.is_valid() and profile_form.is_valid():
+                user = user_form.save()
+                user.set_password(user.password)
+                user.save()
+
+                profile = profile_form.save(commit=False)
+                profile.user = user
+
+                if 'picture' in request.FILES:
+                    profile.picture = request.FILES['picture']
+
+                profile.save()
+                registered = True
+
+            else:
+                print(user_form.errors, profile_form.errors)
+
+    else:
+            # These forms will be blank, ready for user input.
+            user_form = UserForm()
+            profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html',
+                       {'user_form': user_form,
+                        'profile_form': profile_form,
+                        'registered': registered})
