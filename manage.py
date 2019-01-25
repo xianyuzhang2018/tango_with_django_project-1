c306acb03b543d33516194296351ffe498efcd26
Xianyu Zhang <792373462@qq.com>
2019-01-25 22:34:22 +0000

    Renamed project's name, failed

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango_with_jdango_project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango_with_django_project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
